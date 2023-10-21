1735519ca9baa93f405904929392864a9abc30a0
Shiya Shalmali Shetty <137711284+shiyashalmali@users.noreply.github.com>
2023-10-21 10:38:36 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const About=()=>{
+    return(
+        <div>
+            <h1>About </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id distinctio ipsam iure pariatur aperiam inventore natus nam tenetur nostrum facilis.</p>
+            
+        </div>
+    )
+}
+export default About;
---
+++ Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Home=()=>{
+    return(
+        <div>
+            <h1>Home </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id distinctio ipsam iure pariatur aperiam inventore natus nam tenetur nostrum facilis.</p>
+            
+        </div>
+    )
+}
+export default Home;
